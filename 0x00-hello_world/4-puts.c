5e5d9659779f2d5fff83a16f8ae8da613c7b837f
Solomon Iyanda <s4solomon4@gmail.com>
2022-09-08 19:46:31 -0700

    fixed 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-/* main prints to standard output using puts */
+/**
+ * main prints to standard output using puts
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
