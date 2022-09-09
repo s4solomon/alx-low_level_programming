a42070863e4853ae178c7c65dd9e80de87e93f2a
Solomon Iyanda <s4solomon4@gmail.com>
2022-09-08 18:02:25 -0700

    ficed error

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,6 @@ int main(void)
   /*
   * prints to standard output using printf
   */
-  printf ("with proper grammar, but the outcome is a piece of art,\n");
+  printf("with proper grammar, but the outcome is a piece of art,\n");
   return(0);
 }
