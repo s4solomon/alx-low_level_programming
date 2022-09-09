9afd7e3db534e2f9dc54f0bc82c075fdf5c1cb47
Solomon Iyanda <s4solomon4@gmail.com>
2022-09-08 17:48:08 -0700

    ficed error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@ int main(void)
    * prints to standard output using puts
   */
   /* string declaration */
-  str str1[] = "\"Programming is like building a multilingual puzzle";
+  str str1 = "\"Programming is like building a multilingual puzzle";
   puts(str1);
   return(0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,8 +9,8 @@ int main(void)
   long int li;
   long long int lld;
   float f;
-  printf("Size of a char: % byte(s)\n", sizeof(intType));
-  printf("Size of an int: %zu byte(s)\n", sizeof(charType));
+  printf("Size of a char: %zu byte(s)\n", sizeof(c));
+  printf("Size of an int: %zu byte(s)\n", sizeof(i));
   printf("Size of a long int: %ld byte(s)\n", sizeof(li));
   printf("Size of a long long int: %lld byte(s)\n", sizeof(lli));
   printf("Size of a float: %f byte(s)\n", sizeof(f));
