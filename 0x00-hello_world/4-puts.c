c7ce1d37ac71795bdd698a75e1e8705e004292d4
Solomon Iyanda <s4solomon4@gmail.com>
2022-09-08 20:10:56 -0700

    fixed 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
  * main - prints the phrase "Programming is like building a multilingual puzzle
  * Return: 0 if exited properly, non-zero otherwise
  */
@@ -7,4 +7,4 @@ int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-} 
+}
