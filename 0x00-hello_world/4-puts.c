6363b30654e5dad49d5e9f5b90ad21745f65edc1
Solomon Iyanda <s4solomon4@gmail.com>
2022-09-08 19:38:39 -0700

    fixed betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-/**
- * main - prints to standard output using puts
- */
+/* main - prints to standard output using puts */
 int main(void)
 {
-  /* string declaration */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
