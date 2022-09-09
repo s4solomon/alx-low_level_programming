2e11c8d4df4aab81bb07e03443f4db01b219eeea
Solomon Iyanda <s4solomon4@gmail.com>
2022-09-08 18:54:00 -0700

    ficed error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@ int main(void)
    * prints to standard output using puts
   */
   /* string declaration */
-  puts ("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
