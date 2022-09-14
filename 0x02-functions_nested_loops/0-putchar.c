6d00a9c29471030c211a3f390d311533c22b84a1
Solomon Iyanda <s4solomon4@gmail.com>
2022-09-14 21:58:19 +0000

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+/**
+ * main - prints _putchar to the standard output
+ * Return: zero if successful
+ */
+
+/* #include <stdio.h> */
+
+int main(void)
+{
+	int putchar();
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
