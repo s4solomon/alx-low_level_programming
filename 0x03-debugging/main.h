447253f06f5816db442fc647d079b5a580325ddd
Solomon Iyanda <s4solomon4@gmail.com>
2022-09-17 11:51:40 +0000

    revert to previous commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,6 +5,9 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+<<<<<<< HEAD
 void print_remaining_days(int month, int day, int year);
+=======
+>>>>>>> b6d877b71cd008a79d9209ec55465bffd19934d3
 
 #endif /* MAIN_H */
