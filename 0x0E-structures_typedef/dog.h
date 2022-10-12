3707abdc497e3db346002a777ed09bea0bcf2636
Solomon Iyanda <s4solomon4@gmail.com>
2022-10-12 00:07:10 +0000

    Task 0 to 2

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - dog's information
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
