5072dc71b68160136f07090b40855d1ecb6ba1cd
Solomon Iyanda <s4solomon4@gmail.com>
2022-10-21 16:51:16 +0000

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * function: jack_bauer - prints every minutes of the day
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int i, j;
+
+	i = 0;
+
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 64)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
