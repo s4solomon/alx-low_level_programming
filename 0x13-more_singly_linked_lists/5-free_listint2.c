de3273f010c4d9236ae5009a6f3ab7b539ce762c
Solomon Iyanda <s4solomon4@gmail.com>
2022-10-26 22:57:22 +0000

    Task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,11 +12,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
+	if (head == NULL)
+		return;
+
 	while (*head)
 	{
 		tmp = (*head)->next;
 		free(*head);
 		*head = tmp;
 	}
-	*head = NULL;
 }
