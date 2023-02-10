8c07a50537e7f1477e9dc6ca8452f49857c4f962
Solomon Iyanda <s4solomon4@gmail.com>
2023-02-10 07:54:50 -0800

    fixed task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -34,7 +34,7 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 	n_written = write(fd, text_content, strlen(text_content));
-	if (n_written == -1 || sizeof(n_written) != strlen(text_content))
+	if (n_written == -1 || strlen(filename) != strlen(text_content))
 	{
 		close(fd);
 		return (-1);
