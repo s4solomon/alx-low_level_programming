8064aceb9be595213d8a10c15f39ba63c2c66da7
Solomon Iyanda <s4solomon4@gmail.com>
2023-03-17 07:24:39 -0700

    task 1 & 2

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,17 +1,20 @@
 #include <stdlib.h>
-#include "list.h"
+#include <stdio.h>
+#include "lists.h"
 
 /**
-  * dlistint_len - Counts the number of elements in a doubly linked list.
+  * print_dlistint - prints all the elements of a dlistint_t list.
   * @h: The double linked list 2 count.
-  * Return: Number of elements in the doubly linked list.
+  * Return: List of elements in the doubly linked list.
   */
-size_t dlistint_len(const dlistint_t *h)
+
+size_t print_dlistint(const dlistint_t *h)
 {
 	int lenght = 0;
 
 	while (h != NULL)
 	{
+		printf("%d \n", h->n);
 		++lenght;
 		h = h->next;
 	}
